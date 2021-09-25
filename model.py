01452e88a30a37c56825dcc893843c4e76baf003
westermann <lewie@Lewies-MacBook-Pro.local>
2021-09-25 17:20:01 +0200

    minor

--- model.py
+++ model.py
@@ -9,13 +9,12 @@ import glob
 import PIL
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import numpy as np
 
 from tensorflow import keras
-from skimage.io import imread
-from skimage.transform import resize
 
-tf.debugging.set_log_device_placement(True)
+from dataset import Dataset
+
+#tf.debugging.set_log_device_placement(True)
 
 DATA = './data'
 
@@ -24,28 +23,6 @@ label_paths = glob.glob(os.path.join(DATA, 'masks/*.jpg'))
 
 image_tests = [image_paths.pop() for _ in range(5)]
 label_tests = [label_paths.pop() for _ in range(5)]
-
-
-class Dataset(keras.utils.Sequence):
-
-    def __init__(self, batch_size, img_size, img_paths, target_paths):
-        self.batch_size = batch_size
-        self.img_size = img_size
-        self.img_paths = img_paths
-        self.target_paths = target_paths
-
-    def __len__(self):
-        return len(self.img_paths) // self.batch_size
-
-    def __getitem__(self, idx):
-
-        def preprocess_batch(batch):
-            return np.array([resize(imread(filename), (self.img_size, self.img_size))
-                for filename in batch])
-
-        batch_x = self.img_paths[idx * self.batch_size:(idx + 1) * self.batch_size]
-        batch_y = self.target_paths[idx * self.batch_size:(idx + 1) * self.batch_size]
-        return preprocess_batch(batch_x), preprocess_batch(batch_y)
 
 
 def get_model(img_size):
@@ -74,3 +51,4 @@ if __name__ == '__main__':
     model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
     epochs = 5
     model.fit(ds_train, epochs=epochs, validation_data = ds_val)
+    model.save('saved_models/test_model')
